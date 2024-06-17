2ed0028ce82eb9a44c88d30c3e9c3ccdffbb7e87
Sharkdodod <5b0g0004@stust.edu.tw>
2024-06-17 19:04:41 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MySlider from "./MySlider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Javascript程式設計，使用react</h1>
+      <h2>第一個元件</h2>
+      <h1>5B0G0004 | 鄭堤顥</h1>
+      <span>R: </span><MySlider />
+      <span>G: </span><MySlider />
+      <span>B: </span><MySlider />
+      {/* <header className="App-header"> */}
+      {/* <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello React! 2024.5.1</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,8 +21,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+
+      {/* </header> */}
     </div>
   );
 }
