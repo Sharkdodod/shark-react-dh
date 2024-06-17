57896f3381169435e7155aa9090a01c1969c6b53
Sharkdodod <5b0g0004@stust.edu.tw>
2024-06-17 19:19:10 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import MyRGBPanel from "./components/MyRGBPanel";
 import MySlider from "./components/MySlider";
+import MyCalculator from "./components/MyCalculator";
 
 function App() {
   return (
     <div className="App">
       <h1>5B0G0004 | 鄭堤顥</h1>
-      <hr></hr>
       <h1>MySlider</h1>
       <MySlider />
       <h1>MyRGBPanel</h1>
       <MyRGBPanel />
+      <h1>MyCalculator</h1>
+      <MyCalculator />
     </div>
   );
 }
